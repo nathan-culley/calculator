1af3162b0732a6bc920ea4e4b23a73a2d4d3c315
Nathan Culley <nathan@nathanculley.com>
2022-11-02 17:09:34 -0400

    Add event listener to number buttons
    Added an event listener to all number buttons that logs the digit to the console. The calculator does not yet display the numbers on the screen or perform operations.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//functions for basic arithmetic operations
 function add(a,b) {
     return (a + b);
 };
@@ -14,6 +15,7 @@ function divide(a,b) {
     return (a / b);
 };
 
+//calls the arithmetic functions
 function operate(operator, a, b) {
     if (operator == "+") {
         return add(a,b);
@@ -26,5 +28,65 @@ function operate(operator, a, b) {
     }
     if (operator == "/") {
         return divide(a,b);
+    }
+}
+
+//sets variables for each button
+const num9 = document.getElementById('9');
+const num8 = document.getElementById('8');
+const num7 = document.getElementById('7');
+const num6 = document.getElementById('6');
+const num5 = document.getElementById('5');
+const num4 = document.getElementById('4');
+const num3 = document.getElementById('3');
+const num2 = document.getElementById('2');
+const num1 = document.getElementById('1');
+const num0 = document.getElementById('0');
+
+const plus = document.getElementById('add');
+const minus = document.getElementById('subtract');
+const times = document.getElementById('multiply');
+const dividedBy = document.getElementById('divide');
+const backspace = document.getElementById('backspace');
+const period = document.getElementById('period');
+const clear = document.getElementById('clear');
+const equals = document.getElementById('equals');
+
+//sets operand and result variables and current state of calculation
+let firstOperand = "";
+let secondOperand = "";
+let result;
+let firstAssignment = true;
+let secondAssignment = false;
+
+
+//adds event listeners to number buttons
+if (document.addEventListener) {
+    document.addEventListener("click", handleClick, false);
+}
+else if (document.attachEvent) {
+    document.attachEvent("onclick", handleClick);
+}
+function handleClick(event) {
+    event = event || window.event;
+    event.target = event.target || event.srcElement;
+    var element = event.target;
+    // Climb up the document tree from the target of the event
+    while (element) {
+        if (element.nodeName === "BUTTON" && /number/.test(element.className)) {
+            // The user clicked on a <button> or clicked on an element inside a <button>
+            // with a class name called "foo"
+            assignNumber(element);
+            break;
+        }
+        element = element.parentNode;
+    }
+}
+function assignNumber(button) {
+    console.log(button.id);
+    if (secondAssignment = false) {
+        let digit = button.id;
+        console.log(digit);
+
     }
 }
